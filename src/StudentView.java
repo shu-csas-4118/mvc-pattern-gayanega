48a860142d65b7abeab740acf8d205c646b089be
gayanega <35902081+gayanega@users.noreply.github.com>
2018-02-05 20:24:23 -0500

    Update StudentView.java

--- src/StudentView.java
+++ src/StudentView.java
@@ -1,4 +1,3 @@
-
 public class StudentView {
 	private Student student;
 	
@@ -25,5 +24,6 @@ public class StudentView {
 		System.out.println("Student's first name is " + this.student.getFirstName());
 		System.out.println("Student's last name is " + this.student.getLastName());
 		System.out.println("Student's id number is " + this.student.getIdNumber());
+		System.out.println("Student's email is " + this.student.getEmail());
 	}
 }
